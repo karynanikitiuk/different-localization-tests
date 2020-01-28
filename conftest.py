f986626cef92d6e1ef51cdf1e85810fa863dddc6
karynanikitiuk <karynanikitiuk@gmail.com>
2020-01-29 01:46:25 +0700

    add updates to localization repository

--- conftest.py
+++ conftest.py
@@ -2,34 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default='en', help="Choose language: en, es")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.accept_languages': ru, en, es})
-        browser = webdriver.Chrome(options=options)
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-        
-    elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", ru, en, es)
-        browser = webdriver.Firefox(firefox_profile=fp)
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-
-    browser.implicitly_wait(10)
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    browser = webdriver.Chrome(options=options)
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -3,4 +3,5 @@ link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
 def test_guest_should_see_add_to_basket_button(browser):
     browser.get(link)
     browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
-
+    assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
+        "Basket button isn't shown!"
