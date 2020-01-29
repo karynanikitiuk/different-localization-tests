f28db01d33e5142d70be915297a0d23baaa7b552
karynanikitiuk <karynanikitiuk@gmail.com>
2020-01-29 20:01:11 +0700

    some bug fixing

--- test_items.py
+++ test_items.py
@@ -1,7 +1,12 @@
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+import time
+
+#link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
 
 def test_guest_should_see_add_to_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(10)
     browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
     assert browser.find_element_by_class_name('btn-add-to-basket').is_displayed(), \
         "Basket button isn't shown!"
